70c80220e9427c79f5d3da61509af74f8229bdca
qVictor <qqlogin@yandex.ru>
2024-08-20 08:37:03 +0300

    fix tests

--- tests/Downloader/DownloaderTest.php
+++ tests/Downloader/DownloaderTest.php
@@ -8,14 +8,12 @@ use function Hexlet\Downloader\downloadPage;
 
 class DownloaderTest extends TestCase
 {
-    private $url;
-    private $outputPath;
+    private $url = 'http://example.com';
+    private $outputPath = '/tmp/example';
 
     public function setUp(): void
     {
         parent::setUp();
-        $this->url = 'http://example.com';
-        $this->outputPath = '/tmp/example';
     }
 
     public function testDownload()
